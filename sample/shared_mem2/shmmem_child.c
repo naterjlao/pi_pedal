12b7e0d49872147fb136e6a7a4102c24de0f1fe7
Nate Lao <lao.nathan@yahoo.com>
2019-09-22 22:30:42 -0400

    added new shared mem examples

---
+++ sample/shared_mem2/shmmem_child.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
+
+int main() {
+
+	char counter;
+	int memFd; 
+	if ((memFd = shm_open("example_memory", O_RDWR, 0)) == -1) {
+		perror("CHILD: Can't open file\n");
+		return 1;
+	}
+
+	char *buffer; 
+	if ((buffer = mmap(NULL, sizeof(char), PROT_READ | PROT_WRITE, MAP_SHARED, memFd, 0)) == NULL) {
+		perror("CHILD: Can't map\n");
+		return -1;
+	}
+
+	printf("CHILD: writing seconds into shared memory\n");
+	counter = 0;
+	while (counter < 10) {
+		memcpy(buffer,&counter,sizeof(char));
+		sleep(1);
+		counter++;
+	}	
+	
+	memcpy(buffer, "message\0", sizeof(char)*10);
+
+	return 0;
+
+}
---
+++ sample/shared_mem2/shmmem_parent.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+int main() {
+	int memFd = shm_open("example_memory", O_CREAT | O_RDWR, S_IRWXU);
+	if (memFd == -1) {
+		perror("cant open");
+		return 1;
+	} else {
+		//Expand the file to 1 character
+		ftruncate(memFd, sizeof(char) * 2);
+	}
+
+	int pid = fork();
+
+	//Child process
+	if (pid == 0) {
+		printf("Running Child...\n");
+		execlp("./child.x", "./child.x",(char *)NULL);
+	} else {
+		char *buffer = mmap(NULL,sizeof(char)*2, PROT_READ, MAP_SHARED, memFd,0);
+//		buffer[1] = '\0';
+		char current_state = 0;
+		while(waitpid(pid,NULL,WNOHANG) == 0) {
+			if (current_state != *buffer) {
+				printf("%d\n",(int) *buffer);
+				current_state = *buffer;
+			}
+		}
+
+		memFd =	shm_unlink("example_memory");
+		if (memFd == 0) {
+			printf("memory unlinked\n");
+		}
+	}
+
+	return 0;
+}
